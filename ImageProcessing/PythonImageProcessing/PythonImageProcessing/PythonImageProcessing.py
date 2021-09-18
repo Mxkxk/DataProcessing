7c05b11bebaee3adfd94ba9463ec0050ff2a3a65
Fedoriak Maksym <74319751+Mxkxk@users.noreply.github.com>
2021-09-18 18:10:47 +0300

    Fixed logic of i_rgb function

--- ImageProcessing/PythonImageProcessing/PythonImageProcessing/PythonImageProcessing.py
+++ ImageProcessing/PythonImageProcessing/PythonImageProcessing/PythonImageProcessing.py
@@ -7,16 +7,16 @@ lst = []
 size = len(imgMatrix)
 print("Ammount of pixels: " + str(size))
 ii, k = 1, 1
-i_rgb = lambda r, g, b: (int(r*0.2), int(g*0.8), int(b*0.1))
+i_rgb = lambda r, g, b, l0, l1, l2: (int(r*l0), int(g*l1), int(b*l2))
 i_l = lambda r, g, b, l0, l1, l2: r*l0+g*l1+b*l2
 for i in imgMatrix:
 	if (ii/size)*100 > k*10:
 		print("%.2f %% completed..."%((ii/size)*100))
-		k+=1	
+		k+=1
 	#lst.append(i_r(i[0], i[1], i[2]))
-	lst.append(i_l(i[0], i[1], i[2], 0.2, 0.7, 0.1))
+	#lst.append(i_l(i[0], i[1], i[2], 0.4, 0.5, 0.01))
 	ii+=1
 
-newImg = Image.new("L", img.size)
+newImg = Image.new("RGB", img.size)
 newImg.putdata(lst)
 newImg.show()
